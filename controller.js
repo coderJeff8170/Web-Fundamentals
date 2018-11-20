ecbadc5f46bb1837e65ce7f1e789166f106eb9c3
coderJeff8170 <44214906+coderJeff8170@users.noreply.github.com>
2018-11-20 14:58:14 -0500

    Add files via upload

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
 var questionBank = new Array;
 var wordArray = new Array;
@@ -19,38 +19,118 @@ $.getJSON('quizbank.json', function(data) {
 })//getJSON
 
 function titleScreen() {
-	$('#gameContent').append('<div id="gameTitle">HANGMAN</div>
-	<div id="startButton" class="button">BEGIN</div>');
-	$('#startButton').on("click",function (){gameScreen ()});
+	$('#gameContent').append('<div id="gameTitle">HANGMAN</div><div id="startButton" class="button">BEGIN</div>');
+	$('#startButton').on("click",function() {gameScreen()});
 	
 }//display game
+
+function gameScreen() {
+    $('#gameContent').empty();
+    $('#gameContent').append('<div id="pixHolder"><img id="hangman" src="manv2.png"></div>');
+    $('#gameContent').append('<div id="wordHolder"></div>');
+    $('#gameContent').append('<div id="clueHolder"></div>');
+    $('#gameContent').append('<div id="guesses">Previous guesses:</div>');
+    $('#gameContent').append('<div id="feedback"></div>');
+    
+    getWord();
+    var numberOfTiles=currentWord.length;
+    wrongAnswerCount=0
+    previousGuesses=[];
+    
+    for(i=0; i<numberOfTiles;i++){
+    $('#wordHolder').append('<div class="tile" id=t'+i+'></div>');
+    }
+    
+    $('#clueHolder').append("HINT: "+currentClue);
+    
+        $(document).on("keyup", handleKeyUp);
+    
+}//gamescreen
 	
+function getWord(){
+        var rnd=Math.floor(Math.random()*questionBank.length);
+        currentWord=questionBank[rnd] [0];
+        currentClue=questionBank[rnd] [1];
+        questionBank.splice(rnd, 1);
+        wordArray=currentWord.split("");
+}//getWord
+    
+function handleKeyUp (event) {
+    if(event.keyCode>64 && event.keyCode<91) {
+        var found=false;
+        var previouslyEntered=false;
+        var input=String.fromCharCode(event.keyCode).toLowerCase();
+        
+        for(i=0;i<previousGuesses.length;i++){
+            if(input==previousGuesses[i]){
+                previouslyEntered=true;
+            }
+    }
+        
+        if(!previouslyEntered){
+            previousGuesses.push(input);
+            
+            for(i=0;i<wordArray.length;i++){
+            
+                if(input==wordArray[i]){
+                    found=true;
+                    $('#t'+i).append(input);       
+                }
+            
+            }//for
+            
+            if(found){checkAnswer();}
+            else{wrongAnswer(input);}
+            
+        }//if
+        
+    }//if
+     
+}//handlekeyup
+    
+function checkAnswer(){
+    var currentAnswer="";
+    for(i=0;i<currentWord.length;i++){
+        currentAnswer+=($('#t'+i).text());
+    }
+    if(currentAnswer==currentWord){
+        victoryMessage();
+    };
+}//checkAnswer
+    
+function wrongAnswer(a){
+    wrongAnswerCount++;
+    var pos=(wrongAnswerCount*-75) +"px"
+    $('#guesses').append("  "+a);
+    $('#hangman').css("left",pos);
+    if(wrongAnswerCount==6){
+        defeatMessage();
+    }
+}//wronganswer
+    
+function victoryMessage(){
+    $(document).off("keyup", handleKeyUp);
+    $('#feedback').append("CORRECT!<br><br><div id='replay' class='button'>CONTINUE</div>");
+    $('#replay').on("click",function(){
+        if(questionBank.length>0){
+            gameScreen()}
+        else{finalPage()}
+        });
+}//victory
+                  
+function defeatMessage(){
+    $(document).off("keyup", handleKeyUp);
+    $('#feedback').append("You're Dead!<br>(answer= "+ currentWord +")<div id='replay' class='button'>CONTINUE</div>");
+    $('#replay').on("click",function(){
+        if(questionBank.length>0){
+            gameScreen()}
+        else{finalPage()}
+        });
+}//defeat
+
+function finalPage(){
+    $('#gameContent').empty();
+    $('#gameContent').append('div id="finalMessage">You have finished all the words in the game!</div>');
+}//finalpage
 	
-	
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
